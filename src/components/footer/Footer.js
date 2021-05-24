aeeb50096375d6662d729aa108cc24c78b610c89
Oshri Cohen <oshriakaa@gmail.com>
2021-05-24 07:32:09 +0300

    change footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,8 +11,14 @@ const Footer = () => {
       <div className="detail-container">
         <div className="footer-contact-us-container">
           <div className="contact-us-title">צור קשר</div>
-          <div className="contact-phoneNumber">מספר לווצאפ: 053-3555309</div>
-          <div className="contact-email">אימייל: oini@gmail.com</div>
+          <div className="contact-phoneNumber">
+            <i className="fab fa-whatsapp"></i>
+            <div>ווצאפ בלבד: 053-3555309</div>
+          </div>
+          <div className="contact-email">
+            <i className="fas fa-envelope-square"></i>
+            <div>אימייל: oini@gmail.com</div>
+          </div>
         </div>
       </div>
       <div
